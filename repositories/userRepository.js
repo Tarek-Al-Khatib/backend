0d82745b4623514dd9840102c20e634a2b99cce8
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-25 13:53:45 +0200

    Added findByEmailOrUsername and create function for user

---
+++ repositories/userRepository.js
@@ -0,0 +1,13 @@
+import UserModel from "../models/main.js";
+
+export const userRepository = {
+  async findByEmailOrUsername(email, username) {
+    return UserModel.findFirst({
+      where: { OR: [{ email }, { username }] },
+    });
+  },
+
+  async createUser(user) {
+    return UserModel.create({ data: user });
+  },
+};
