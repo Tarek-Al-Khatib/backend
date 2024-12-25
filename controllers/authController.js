4a35f8f5962d7989379e8ed90962373b0bfec749
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-25 17:17:42 +0200

    Added login function to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,3 +51,44 @@ export const register = async (req, res) => {
       .json({ error: "An error occurred while registering the user." });
   }
 };
+
+export const login = async (req, res) => {
+  const { emailOrUsername, password } = req.body;
+
+  try {
+    const user = await userRepository.findByEmailOrUsername(
+      emailOrUsername,
+      username
+    );
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid credentials." });
+    }
+
+    const token = jwtHelper.generateToken({
+      userId: user.id,
+      email: user.email,
+      level: user.level,
+      points: user.points,
+    });
+
+    res.status(200).json({
+      message: "Login successful.",
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        level: user.level,
+        points: user.points,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error("Error in login:", error);
+    res.status(500).json({ error: "An error occurred while logging in." });
+  }
+};
