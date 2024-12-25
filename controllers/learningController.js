82395314523357e866c0d14ef15990be423377f9
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-25 20:17:30 +0200

    Added addPlan to learningController

--- controllers/learningController.js
+++ controllers/learningController.js
@@ -11,3 +11,19 @@ export const getPlans = async (req, res) => {
     res.status(500).json({ error: "An error occurred while fetching plans." });
   }
 };
+
+export const addPlan = async (req, res) => {
+  const userId = req.params.userId;
+  const planData = req.body;
+
+  try {
+    const newPlan = await learningRepository.addPlan(planData, userId);
+    res.status(200).json({
+      message: "Plan added successfully.",
+      plan: newPlan,
+    });
+  } catch (error) {
+    console.error("Error in addPlan:", error);
+    res.status(500).json({ error: "An error occurred while adding the plan." });
+  }
+};
