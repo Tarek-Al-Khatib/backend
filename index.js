0a79174140a3d3a79ba50b5c9e1d164d724917d5
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-26 20:49:40 +0200

    Used the routes for community controller

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import app from "./app/app.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoute.js";
+import communityRoutes from "./routes/communityRoute.js";
 import learningRoutes from "./routes/learningRoute.js";
 
 dotenv.config();
@@ -8,6 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/community", communityRoutes);
 app.use("/api/learning", learningRoutes);
 
 app.listen(PORT, () => {
