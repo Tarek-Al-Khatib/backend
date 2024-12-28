84d6870763a62e41e42c11b774cb1e17605bc90b
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-28 16:06:05 +0200

    Added getUserCommunities in community controller

--- controllers/communityController.js
+++ controllers/communityController.js
@@ -91,9 +91,21 @@ export const joinCommunity = async (req, res) => {
       communityId,
       role
     );
-    res.status(201).json(communityMember);
+    res.status(200).json(communityMember);
   } catch (error) {
     console.error("Error in joinCommunity:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getUserCommunities = async (req, res) => {
+  const userId = Number(req.params.userId);
+
+  try {
+    const communities = await communityRepository.getUserCommunities(userId);
+    res.status(200).json(communities);
+  } catch (error) {
+    console.error("Error in getUserCommunities:", error);
     res.status(500).json({ error: error.message });
   }
 };
