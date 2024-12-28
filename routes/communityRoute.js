aa6eb85cbddf3595238a729cce765597d3e89a87
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-28 16:19:07 +0200

    Added routes to communityRoute

--- routes/communityRoute.js
+++ routes/communityRoute.js
@@ -3,13 +3,21 @@ import {
   getChannels,
   getChats,
   getMembers,
+  createCommunity,
+  createChannel,
+  joinCommunity,
+  getUserCommunities,
 } from "../controllers/communityController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+router.post("/", authMiddleware, createCommunity);
+router.post("/:communityId/channels", authMiddleware, createChannel);
+router.post("/:communityId/join", authMiddleware, joinCommunity);
 router.get("/:communityId/channels", authMiddleware, getChannels);
 router.get("/:channelId/chats", authMiddleware, getChats);
 router.get("/:communityId/members", authMiddleware, getMembers);
+router.get("/:userId/communities", authMiddleware, getUserCommunities);
 
 export default router;
