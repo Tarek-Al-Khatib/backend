8231be9e6645f9277713331c8c2356a86a2c1244
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-31 05:28:48 +0200

    Changed structure of socket file to a function

--- websocket/socket.js
+++ websocket/socket.js
@@ -1,39 +1,35 @@
 import { Server } from "socket.io";
-import http from "http";
-import app from "../app/app.js";
 import { communityRepository } from "../repositories/communityRepository.js";
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected", socket.id);
-
-  socket.on("joinChannel", (channelId) => {
-    socket.join(channelId);
-    console.log(`User ${socket.id} joined channel ${channelId}`);
+export function createSocketServer(httpServer) {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
   });
 
-  socket.on("sendMessage", async ({ channelId, userId, message }) => {
-    try {
-      await communityRepository.saveMessage(channelId, userId, message);
-      io.to(channelId).emit("receiveMessage", message);
-    } catch (error) {
-      console.error("Error saving message:", error);
-    }
+  io.on("connection", (socket) => {
+    console.log("User connected", socket.id);
+
+    socket.on("joinChannel", (channelId) => {
+      socket.join(channelId);
+      console.log(`User ${socket.id} joined channel ${channelId}`);
+    });
+
+    socket.on("sendMessage", async ({ channelId, userId, message }) => {
+      try {
+        await communityRepository.saveMessage(channelId, userId, message);
+        io.to(channelId).emit("receiveMessage", message);
+      } catch (error) {
+        console.error("Error saving message:", error);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
-
-server.listen(1738, () => {
-  console.log("Server running on port 1738");
-});
+  return io;
+}
