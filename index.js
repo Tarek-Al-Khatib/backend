8a876ba94688cfdc1fd6a539212dbd29b961909b
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-01 04:54:36 +0200

    Used notification route

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ createSocketServer(server);
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/notifications", notificationRoutes);
 app.use("/api/community", communityRoutes);
 app.use("/api/learning", learningRoutes);
 
