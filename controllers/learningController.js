c310b6f69daf3698428931fd74e59900d22a04b5
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 19:36:28 +0200

    Added getusersteps to learning controller

--- controllers/learningController.js
+++ controllers/learningController.js
@@ -88,3 +88,22 @@ export const markStepAsDone = async (req, res) => {
       .json({ error: "An error occurred while marking the step as done." });
   }
 };
+
+export const getUserStepsCompletedLastWeek = async (req, res) => {
+  const userId = Number(req.params.userId);
+
+  try {
+    const steps = await learningRepository.getStepsCompletedLastWeek(userId);
+
+    return res.status(200).json({
+      message: "Steps completed in the last week fetched successfully",
+      data: steps,
+    });
+  } catch (error) {
+    console.error("Error fetching user steps completed last week:", error);
+    return res.status(500).json({
+      message:
+        "An error occurred while fetching user steps completed last week",
+    });
+  }
+};
