10f8bd7abd6a7d924bd82b179a67247d229f58ff
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 08:34:43 +0200

    Added update interview route

--- routes/interviewRoute.js
+++ routes/interviewRoute.js
@@ -3,6 +3,8 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
   getUserInterviews,
   getInterviewInvitations,
+  createInterview,
+  updateInterview,
 } from "../controllers/interviewController.js";
 
 const router = express.Router();
@@ -10,4 +12,5 @@ const router = express.Router();
 router.get("/", authMiddleware, getUserInterviews);
 router.get("/invitations", authMiddleware, getInterviewInvitations);
 router.post("/", authMiddleware, createInterview);
+router.put("/:interviewId", authMiddleware, updateInterview);
 export default router;
