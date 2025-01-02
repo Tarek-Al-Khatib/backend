dd894663f080bb6d06943ad443791c44566f47f8
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 05:26:03 +0200

    Added getProfile to user controller

--- controllers/personalUserController.js
+++ controllers/personalUserController.js
@@ -17,3 +17,21 @@ export const getLeaderboardByPoints = async (req, res) => {
     });
   }
 };
+
+export const getProfile = async (req, res) => {
+  const userId = Number(req.params.userId);
+
+  try {
+    const profileData = await userRepository.getProfile(userId);
+
+    return res.status(200).json({
+      message: "Profile fetched successfully",
+      data: profileData,
+    });
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    return res.status(500).json({
+      message: "An error occurred while fetching the profile",
+    });
+  }
+};
