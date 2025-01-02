312d6c87bcf37113bad5dbf53238349202c2cb06
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 05:32:35 +0200

    Fixed import bug

--- routes/personalUserRoute.js
+++ routes/personalUserRoute.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { userController } from "../controllers/userController.js";
+import {
+  getLeaderboardByPoints,
+  getProfile,
+} from "../controllers/personalUserController.js";
 
 const router = express.Router();
 
-router.get(
-  "/leaderboard",
-  authMiddleware,
-  userController.getLeaderboardByPoints
-);
-router.get("/profile", authMiddleware, userController.getProfile);
+router.get("/leaderboard", authMiddleware, getLeaderboardByPoints);
+router.get("/profile", authMiddleware, getProfile);
 
 export default router;
