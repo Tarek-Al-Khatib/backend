07ff09bec83a1e2b93360eb6f8dc4fc6d3f7f77d
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 07:37:08 +0200

    Added getInterviewInvitations to interview controller

--- controllers/interviewController.js
+++ controllers/interviewController.js
@@ -1,10 +1,10 @@
-import { personalUserRepository } from "../repositories/personalUserRepository.js";
+import { interviewRepository } from "../repositories/interviewRepository";
 
 export const getUserInterviews = async (req, res) => {
   const userId = Number(req.params.userId);
 
   try {
-    const interviews = await personalUserRepository.getUserInterviews(userId);
+    const interviews = await interviewRepository.getUserInterviews(userId);
 
     return res.status(200).json({
       message: "Interviews fetched successfully",
@@ -14,6 +14,26 @@ export const getUserInterviews = async (req, res) => {
     console.error("Error fetching interviews:", error);
     return res.status(500).json({
       message: "An error occurred while fetching the interviews",
+    });
+  }
+};
+
+export const getInterviewInvitations = async (req, res) => {
+  const userId = Number(req.params.userId);
+
+  try {
+    const invitations = await interviewRepository.getInterviewInvitations(
+      userId
+    );
+
+    return res.status(200).json({
+      message: "Interview invitations fetched successfully",
+      data: invitations,
+    });
+  } catch (error) {
+    console.error("Error fetching interview invitations:", error);
+    return res.status(500).json({
+      message: "An error occurred while fetching interview invitations",
     });
   }
 };
