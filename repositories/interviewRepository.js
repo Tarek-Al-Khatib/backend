c8fab934675cf04125c0149dae208bbb92ebf63b
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-02 07:33:54 +0200

    Added getUserInterviews func to interview repo

--- repositories/interviewRepository.js
+++ repositories/interviewRepository.js
@@ -17,4 +17,27 @@ export const interviewRepository = {
 
     return interviews;
   },
+  async getUserInterviews(userId) {
+    const interviews = await InterviewModel.findMany({
+      where: {
+        user_id: userId,
+        date: { lte: new Date() },
+      },
+      include: {
+        moderator: true,
+      },
+    });
+
+    return interviews.map((interview) => ({
+      id: interview.id,
+      date: interview.date,
+      interviewer: interview.moderator?.username || "AI",
+      feedback: interview.feedback || "Pending",
+      status:
+        interview.feedback || interview.points === 0
+          ? "Pending"
+          : interview.status,
+      points: interview.points || "Pending",
+    }));
+  },
 };
