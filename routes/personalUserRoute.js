c8dcf90b449cd606a980e796ee1b4572a7d4a0b8
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-04 09:02:54 +0200

    Added get user by id to the route

--- routes/personalUserRoute.js
+++ routes/personalUserRoute.js
@@ -3,11 +3,13 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
   getLeaderboardByPoints,
   getProfile,
+  getUserById,
 } from "../controllers/personalUserController.js";
 
 const router = express.Router();
 
 router.get("/leaderboard", authMiddleware, getLeaderboardByPoints);
 router.get("/profile", authMiddleware, getProfile);
+router.get("/:userId", authMiddleware, getUserById);
 
 export default router;
