2272964e60fa9072ad788025311ec26f0c2ea58f
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-04 13:53:06 +0200

    Added file filter to multer middleware

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -9,3 +9,12 @@ const storage = multer.diskStorage({
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only JPEG, JPG, and PNG files are allowed"), false);
+  }
+};
