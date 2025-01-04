8dd35ace5eaee0b80b4274683d4421cd5d1bbaf7
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-04 11:45:31 +0200

    Added return statement in formidableMiddleware

--- middlewares/formidableMiddlware.js
+++ middlewares/formidableMiddlware.js
@@ -5,8 +5,22 @@ const formidableMiddleware = (options = {}) => {
     multiples: options.multiples || true,
     uploadDir: options.uploadDir || "./uploads",
     keepExtensions: options.keepExtensions || true,
-    maxFileSize: options.maxFileSize || 5 * 1024 * 1024,
+    maxFileSize: options.maxFileSize || 10 * 1024 * 1024,
   });
+
+  return (req, res, next) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        console.error("Error in formidable parse:", err);
+        return res.status(400).json({ error: "Invalid form data" });
+      }
+
+      req.body = fields;
+      req.files = files;
+
+      next();
+    });
+  };
 };
 
 export default formidableMiddleware;
