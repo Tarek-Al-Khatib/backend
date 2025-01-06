2fe406978d6d8576dee07fd0ce689cf7a2e64aec
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-06 02:42:33 +0200

    Used getUserSteps in learning route

--- routes/learningRoute.js
+++ routes/learningRoute.js
@@ -5,6 +5,7 @@ import {
   markPlanAsDone,
   markStepAsDone,
   getPlans,
+  getUserStepsCompletedLastWeek,
 } from "../controllers/learningController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
@@ -15,5 +16,6 @@ router.post("/:userId", authMiddleware, addPlan);
 router.put("/:planId", authMiddleware, updatePlan);
 router.put("/plan-done/:planId", authMiddleware, markPlanAsDone);
 router.put("/step-done/:stepId", authMiddleware, markStepAsDone);
+router.get("/last-week", authMiddleware, getUserStepsCompletedLastWeek);
 
 export default router;
