878fbd976fb514c6f5f78a9fd8a3f9a89b1b47cc
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-08 21:02:27 +0200

    Fixed level updating on incrementUserPoints

--- utils/IncrementUserPoints.js
+++ utils/IncrementUserPoints.js
@@ -3,14 +3,22 @@ import { UserModel } from "../models/main.js";
 export const incrementUserPoints = async (userId, points) => {
   try {
     const user = await UserModel.findFirstOrThrow({ where: { id: userId } });
-    let updatedLevel;
     if (user) {
-      updatedLevel = (user.points + points) / (user.level * 50);
+      const newPoints = user.points + points;
+      const levelThreshold = user.level * 50;
+
+      while (newPoints >= levelThreshold) {
+        user.level += 1;
+        levelThreshold = user.level * 50;
+      }
+
+      user.points = newPoints;
     }
+
     await UserModel.update({
       data: {
-        points: { increment: points },
-        level: updatedLevel,
+        points: user.points,
+        level: user.level,
       },
       where: {
         id: userId,
