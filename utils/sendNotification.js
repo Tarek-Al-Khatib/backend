0b6a634640cfe668fac6e99a2d99a0eab569fb98
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-08 20:34:11 +0200

    Fixed bug on send notification function

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,5 +1,5 @@
 import { io } from "../index.js";
 
-export const sendnotification = (type, message) => {
+export const sendnotification = (userId, type, message) => {
   io.to(`user-${userId}`).emit("receiveNotification", { type, message });
 };
