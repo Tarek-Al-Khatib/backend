11ed1c7d2170aab6728ce5094b8bea316682ff0c
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-08 20:36:43 +0200

    Added notification creation when a notification is sent

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,5 +1,16 @@
 import { io } from "../index.js";
+import { notificationRepository } from "../repositories/notificationsRepository.js";
 
-export const sendnotification = (userId, type, message) => {
-  io.to(`user-${userId}`).emit("receiveNotification", { type, message });
+export const sendnotification = async (userId, type, message) => {
+  try {
+    const notification = await notificationRepository.createNotification({
+      message,
+      type,
+      userId,
+    });
+
+    io.to(`user-${userId}`).emit("receiveNotification", notification);
+  } catch (error) {
+    console.error("Error in createNotification:", error);
+  }
 };
