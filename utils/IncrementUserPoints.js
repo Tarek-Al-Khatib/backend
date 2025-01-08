ee9ad4f8fff4a48781e4131291c25809c9c32f95
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-08 19:32:23 +0200

    Added updating level of the user

--- utils/IncrementUserPoints.js
+++ utils/IncrementUserPoints.js
@@ -2,9 +2,15 @@ import { UserModel } from "../models/main";
 
 export const incrementUserPoints = async (userId, points) => {
   try {
+    const user = await UserModel.findFirstOrThrow({ where: { id: userId } });
+    let updatedLevel;
+    if (user) {
+      updatedLevel = (user.points + points) / (user.level * 50);
+    }
     await UserModel.update({
       data: {
         points: { increment: points },
+        level: updatedLevel,
       },
       where: {
         id: userId,
