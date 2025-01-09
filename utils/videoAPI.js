968a727e3ed0ad52b743ad3639004b8720eac30b
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-09 09:00:27 +0200

    Added get room function to video api

--- utils/videoAPI.js
+++ utils/videoAPI.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
 
@@ -7,5 +8,20 @@ const API_KEY = process.env.DAILY_API_KEY;
 const headers = {
   Accept: "application/json",
   "Content-Type": "application/json",
-  Authorization: "Bearer " + API_KEY,
+  Authorization: `Bearer ${API_KEY}`,
+};
+
+export const getRoom = async (room) => {
+  try {
+    const response = await fetch(`https://workwise.daily.co/v1/rooms/${room}`, {
+      method: "GET",
+      headers,
+    });
+    response = await response.json();
+
+    return response;
+  } catch (error) {
+    console.log("Error fetchin room in get room func:", error);
+    return error;
+  }
 };
