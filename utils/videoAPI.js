d218ccf71aaf27406834ffe25b3ce61c85885c2c
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-09 09:03:46 +0200

    Added create room function in video api

--- utils/videoAPI.js
+++ utils/videoAPI.js
@@ -21,7 +21,32 @@ export const getRoom = async (room) => {
 
     return response;
   } catch (error) {
-    console.log("Error fetchin room in get room func:", error);
+    console.log("Error fetching room in get room func:", error);
+    return error;
+  }
+};
+
+export const createRoom = async (room) => {
+  try {
+    const response = await fetch("https://workwise.daily.co/v1/rooms", {
+      method: "POST",
+      headers,
+      body: JSON.stringify({
+        name: room,
+        properties: {
+          enable_screenshare: true,
+          enable_chat: true,
+          start_video_off: true,
+          start_audio_off: false,
+          lang: "en",
+        },
+      }),
+    });
+    response = await response.json();
+
+    return response;
+  } catch (error) {
+    console.log("Error creating room in create room func:", error);
     return error;
   }
 };
