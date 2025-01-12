39f72a266a0acdc9488e24d2d2de54621a1a12f4
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-12 08:12:36 +0200

    Updated the interview table

---
+++ prisma/migrations/20250112061220_interview_adjustment/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `score` on the `interviews` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `interviews` DROP FOREIGN KEY `Interviews_moderator_id_fkey`;
+
+-- DropIndex
+DROP INDEX `Interviews_moderator_id_fkey` ON `interviews`;
+
+-- AlterTable
+ALTER TABLE `interviews` DROP COLUMN `score`,
+    MODIFY `moderator_id` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `Interviews` ADD CONSTRAINT `Interviews_moderator_id_fkey` FOREIGN KEY (`moderator_id`) REFERENCES `Users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
