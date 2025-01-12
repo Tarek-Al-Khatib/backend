7ef6ddd09ef056ae8eb092358c0351104f7a69e8
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-12 09:40:11 +0200

    Added enhance plan to ai controller

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -204,32 +204,6 @@ export const completedAiInterview = async (req, res) => {
 
     let feedbackResponse = completion.choices[0].message;
     let interviewFeedback = JSON.parse(feedbackResponse.content);
-
-    // model Interviews {
-    //   id           Int            @id @default(autoincrement())
-    //   type         TypeInterviews
-    //   feedback     String?        @db.VarChar(500)
-    //   date         DateTime
-    //   status       Status         @default(PENDING)
-    //   created_at   DateTime       @default(now())
-    //   points       Int            @default(0)
-    //   user_id      Int
-    //   moderator_id Int
-    //   completed_at DateTime?s
-
-    //   user      Users @relation(name: "InterviewedUser", fields: [user_id], references: [id])
-    //   moderator Users @relation(name: "InterviewModerator", fields: [moderator_id], references: [id])
-    // }
-    // enum TypeInterviews {
-    //   MODERATOR
-    //   AI
-    // }
-
-    // enum Status {
-    //   PENDING
-    //   ACCEPTED
-    //   REJECTED
-    // }
     const interview = await interviewRepository.createInterview({
       type: "AI",
       feedback: interviewFeedback.feedback,
@@ -242,5 +216,59 @@ export const completedAiInterview = async (req, res) => {
     res
       .status(200)
       .json({ message: interviewFeedback, interviewCreated: interview });
+  }
+};
+
+export const enhancePlan = async (req, res) => {
+  const learningPlan = req.body.plan;
+  try {
+    if (openai.apiKey) {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        response_format: {
+          type: "json_object",
+        },
+        messages: [
+          {
+            role: "system",
+            content: `
+            You are a learning plan enhancer.
+            You will always reply with one JSON formatted message.
+            The message contains the plan which has title (max 100 char), description (max 300 char), and array of steps.
+            The steps are an array of objects which has step_title (max 50 char), and step_description (max 300 char) properties for each object
+            If the user has already set a title and description for the plan, enhance it by improving the titles of the plan and description.
+            The learning plans are almost 7-15 steps.
+            `,
+          },
+          {
+            role: "system",
+            content: `The existing learning plan that the user created:
+            title: ${learningPlan.title ? learningPlan.title : "No Title"}
+            description: ${
+              learningPlan.description
+                ? learningPlan.description
+                : "No description"
+            }
+            steps: {${learningPlan.steps.map((step) => {
+              return `step_title: ${
+                step.step_title ? step.step_title : "No Title for step"
+              }
+                      step_description: ${
+                        step.step_description
+                          ? step.step_description
+                          : "No description for step"
+                      }`;
+            })}
+          },`,
+          },
+        ],
+      });
+
+      let enhacementResponse = completion.choices[0].message;
+      let enhancedPlan = JSON.parse(enhacementResponse.content);
+      res.status(200).json({ plan: enhancedPlan });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
