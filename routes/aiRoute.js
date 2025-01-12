251684ec3d2055f43c443a0630578b6d53d8f5e5
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-12 09:40:21 +0200

    Routed the enhance plan function

--- routes/aiRoute.js
+++ routes/aiRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
   completedAiInterview,
+  enhancePlan,
   interviewChat,
 } from "../controllers/aiController.js";
 
@@ -9,4 +10,5 @@ const router = express.Router();
 
 router.post("/interview", authMiddleware, interviewChat);
 router.post("/completed", authMiddleware, completedAiInterview);
+router.post("/enhance", enhancePlan);
 export default router;
